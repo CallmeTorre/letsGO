b12a0023a3989cbcc991e84b8840cf7f34238f3f
Alexis Torreblanca <alexis_torreblanca@outlook.com>
2020-09-30 13:13:08 -0500

    Added figures interfaces

---
+++ interfaces/figures.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type square struct {
+	sideLength float64
+}
+type triangle struct {
+	heigth float64
+	base   float64
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func (t triangle) getArea() float64 {
+	return t.base * t.heigth * .5
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
+func main() {
+	t := triangle{
+		heigth: 12.5,
+		base:   10.9}
+	s := square{
+		sideLength: 12.5}
+	printArea(t)
+	printArea(s)
+}
