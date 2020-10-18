8040011e12a4e675c068bf2b80bd3943ac5818d8
Alexis Torreblanca <alexis_torreblanca@outlook.com>
2020-10-18 18:33:44 -0500

    Added logrus library

--- api/app/app.go
+++ api/app/app.go
@@ -1,8 +1,7 @@
 package app
 
 import (
-	"log"
-
+	"github.com/CallmeTorre/letsGO/api/log"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -12,14 +11,16 @@ var router *gin.Engine
 func init() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Fatal("Error loading .env file", err, "status:fail")
 	}
 	router = gin.Default()
 	//router = gin.New() //Blank engine without middleware
 }
 
 func StartApp() {
+	log.Info("About to map URLS", "step:1", "status:pending")
 	mapUrls()
+	log.Info("URLS successfully mapped", "step:2", "status:success")
 	if err := router.Run(":8080"); err != nil {
 		panic(err)
 	}
---
+++ api/log/logger.go
@@ -0,0 +1,67 @@
+package log
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/CallmeTorre/letsGO/api/config"
+	"github.com/sirupsen/logrus"
+)
+
+var Log *logrus.Logger
+
+func init() {
+	level, err := logrus.ParseLevel(config.LogLevel)
+	if err != nil {
+		level = logrus.DebugLevel
+	}
+	Log = &logrus.Logger{
+		Level: level,
+		Out:   os.Stdout,
+	}
+	if config.IsProduction() {
+		Log.Formatter = &logrus.JSONFormatter{}
+	} else {
+		Log.Formatter = &logrus.TextFormatter{}
+	}
+}
+
+func Info(message string, tags ...string) {
+	if Log.Level < logrus.InfoLevel {
+		return
+	}
+	Log.WithFields(parseFields(tags...)).Info(message)
+}
+
+func Debug(message string, tags ...string) {
+	if Log.Level < logrus.DebugLevel {
+		return
+	}
+	Log.WithFields(parseFields(tags...)).Debug(message)
+}
+
+func Error(message string, err error, tags ...string) {
+	if Log.Level < logrus.ErrorLevel {
+		return
+	}
+	message = fmt.Sprintf("%s - ERROR - %s", message, err.Error())
+	Log.WithFields(parseFields(tags...)).Error(message)
+}
+
+func Fatal(message string, err error, tags ...string) {
+	if Log.Level < logrus.FatalLevel {
+		return
+	}
+	message = fmt.Sprintf("%s - ERROR - %s", message, err.Error())
+	Log.WithFields(parseFields(tags...)).Fatal(message)
+}
+
+func parseFields(tags ...string) logrus.Fields {
+	result := make(logrus.Fields, len(tags))
+	for _, tag := range tags {
+		elem := strings.Split(tag, ":")
+		result[strings.TrimSpace(elem[0])] = strings.TrimSpace(elem[1])
+	}
+	return result
+}
